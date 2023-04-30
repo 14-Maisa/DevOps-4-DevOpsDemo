3b5006aebdc3451ef30699a136b2fc57dca509b1
14-Maisa <ahmadmai@students.zhaw.ch>
2023-04-30 12:13:33 +0200

    git

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/MengenrabattTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/MengenrabattTest.java
@@ -6,28 +6,11 @@ import org.junit.jupiter.api.Test;
 
 public class MengenrabattTest {
     
-    @Test 
-    public void MengenRabatt1(){
-        Mengenrabatt rabatt = new Mengenrabatt();
-        Assertions.assertEquals("Mengenrabatt nicht erhalten", rabatt.convert(1));
-    }
-
-    @Test 
-    public void MengenRabatt18(){
-        Mengenrabatt rabatt = new Mengenrabatt();
-        Assertions.assertEquals("3.6 % Mengenrabatt erhalten", rabatt.convert(18));
-    }
 
     @Test 
     public void MengenRabatt57(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("11.4 % Mengenrabatt erhalten", rabatt.convert(57));
-    }
-
-    @Test 
-    public void MengenRabatt103(){
-        Mengenrabatt rabatt = new Mengenrabatt();
-        Assertions.assertEquals("Mehr als 100 Artikeln können nicht bestellt werden", rabatt.convert(103));
     }
 
     @Test 
@@ -51,26 +34,7 @@ public class MengenrabattTest {
     @Test 
     public void MengenRabatt99(){
         Mengenrabatt rabatt = new Mengenrabatt();
-        Assertions.assertEquals("19.8 % Mengenrabatt erhalten", rabatt.convert(99));
+        Assertions.assertEquals("20 % Mengenrabatt erhalten", rabatt.convert(99));
     }
-
-    @Test 
-    public void MengenRabatt5(){
-        Mengenrabatt rabatt = new Mengenrabatt();
-        Assertions.assertEquals("1 % Mengenrabatt erhalten", rabatt.convert(5));
-    }
-
-    @Test 
-    public void MengenRabatt75(){
-        Mengenrabatt rabatt = new Mengenrabatt();
-        Assertions.assertEquals("15 % Mengenrabatt erhalten", rabatt.convert(75));
-    }
-
-    @Test 
-    public void MengenRabatt0(){
-        Mengenrabatt rabatt = new Mengenrabatt();
-        Assertions.assertEquals("Mengenrabatt nicht erhalten", rabatt.convert(0));
-    }
-
 
 }
