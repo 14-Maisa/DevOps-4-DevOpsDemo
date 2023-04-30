1289f31a89e3839e6000cdc2b3f6fd39fef3c0db
14-Maisa <ahmadmai@students.zhaw.ch>
2023-04-30 12:54:44 +0200

    public entfernt

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/MengenrabattTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/MengenrabattTest.java
@@ -8,31 +8,31 @@ public class MengenrabattTest {
     
 
     @Test 
-    public void MengenRabatt57(){
+    void MengenRabatt57(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("11.4 % Mengenrabatt erhalten", rabatt.convert(57));
     }
 
     @Test 
-    public void MengenRabatt22(){
+    void MengenRabatt22(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("4.4 % Mengenrabatt erhalten", rabatt.convert(22));
     }
 
     @Test 
-    public void MengenRabatt37(){
+    void MengenRabatt37(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("7.4 % Mengenrabatt erhalten", rabatt.convert(37));
     }
 
     @Test 
-    public void MengenRabatt28(){
+    void MengenRabatt28(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("6 % Mengenrabatt erhalten", rabatt.convert(28));
     }
 
     @Test 
-    public void MengenRabatt99(){
+    void MengenRabatt99(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("20 % Mengenrabatt erhalten", rabatt.convert(99));
     }
