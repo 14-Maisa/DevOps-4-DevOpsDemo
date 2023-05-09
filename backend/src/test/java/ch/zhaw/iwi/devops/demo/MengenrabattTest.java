f4bf1a6f6a2b9aa190a073e86d54da9b416da980
14-Maisa <93995465+14-Maisa@users.noreply.github.com>
2023-05-09 18:15:09 +0200

    Update MengenrabattTest.java

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/MengenrabattTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/MengenrabattTest.java
@@ -8,31 +8,31 @@ public class MengenrabattTest {
     
 
     @Test 
-    void MengenRabatt57(){
+    public void MengenRabatt57(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("11.4 % Mengenrabatt erhalten", rabatt.convert(57));
     }
 
     @Test 
-    void MengenRabatt22(){
+    public void MengenRabatt22(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("4.4 % Mengenrabatt erhalten", rabatt.convert(22));
     }
 
     @Test 
-    void MengenRabatt37(){
+    public void MengenRabatt37(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("7.4 % Mengenrabatt erhalten", rabatt.convert(37));
     }
 
     @Test 
-    void MengenRabatt28(){
+    public void MengenRabatt28(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("6 % Mengenrabatt erhalten", rabatt.convert(28));
     }
 
     @Test 
-    void MengenRabatt99(){
+    public void MengenRabatt99(){
         Mengenrabatt rabatt = new Mengenrabatt();
         Assertions.assertEquals("20 % Mengenrabatt erhalten", rabatt.convert(99));
     }
